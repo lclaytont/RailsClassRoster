6aa84395c6b577def2269579772a9dd5e2e82634
Clay Terry <lclaytont@gmail.com>
2018-08-04 15:31:57 +0000

    New commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,11 @@ class UsersController < ApplicationController
     def index
         @users = User.all
     end
+    
+    # GET /users/1
+    def show 
+        # Which ActiveRecord Method searches by id?
+        @user = User.find(params[:id])
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'hello' => 'static#hello'
+  
   get 'users' => 'users#index'
+  get 'users/:id' => 'users#show'
 end
+
